cbed998a81933a059da986d39854da636295c591
Lala-banners <lala.l.banno@hotmail.com>
2021-02-22 22:52:54 +1100

    Menu is 90% done

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+namespace PetTime.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        #region UI Elements
+        public string LoadScene = "PetSimulator";
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void StartGame(int sceneIndex)
+        {
+            EditorSceneManager.LoadScene(sceneIndex);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("Quitting Game");
+#if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+#endif
+            Application.Quit();
+        }
+    }
+}
