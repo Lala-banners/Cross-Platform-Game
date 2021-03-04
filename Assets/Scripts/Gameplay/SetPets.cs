691b3b1844bb75d3e1fb4473fb450509535d7fdf
Lala-banners <lala.l.banno@hotmail.com>
2021-03-04 13:34:39 +1100

    random

--- Assets/Scripts/Gameplay/SetPets.cs
+++ Assets/Scripts/Gameplay/SetPets.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SetPets : SingletonBase<SetPets>
+public class SetPets : MonoBehaviour
 {
     //Array of Pets
 
